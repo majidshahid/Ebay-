9e16504b39edb885e190e254fb5fe2d0e9321b6f
majid shahid <majidshahid474@gmail.com>
2021-10-22 20:41:21 -0700

    js

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+import "../footer/style.css"
+function Footer (){
+  return(
+    <div>
+    <h1>COPY RIGHT </h1>
+</div>
+  )
+}
+export default Footer;
--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,11 +1,19 @@
 
 import {Modal,Card } from "../../components";
 import First from "../../assets/images/1st.jpg"; 
+import Footer  from "../../components/footer";
  function Home() {
     return (
         <div>
             <Modal/>
+            <div className="row"  style={{padding: "5%"}}>
 <Card pic={First} text="the theory which explain the arrangement of three state of matter"/>
+<Card pic={First} text="the theory which explain the arrangement of three state of matter"/>
+<Card pic={First} text="the theory which explain the arrangement of three state of matter"/>
+<Card pic={First} text="the theory which explain the arrangement of three state of matter"/>
+<Card pic={First} text="the theory which explain the arrangement of three state of matter"/>
+</div>
+<Footer/>
         </div>
     )
 }
